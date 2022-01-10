09d9439eaada0aaff841f58ad721169631eb43f4
Francesca Obordo <fobordo@gmail.com>
2022-01-10 14:40:28 -0500

    Created the handleClick() function.

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,13 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+}
